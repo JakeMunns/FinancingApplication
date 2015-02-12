7277395e924b25ff31d281431150ed7482f3ece4
Emma <emmajanefearn@aol.com>
2015-02-12 01:29:43 +0000

    New Menu System
    Menu system now works

--- app/src/main/java/emmajanefearn/financingapplication/Menu.java
+++ app/src/main/java/emmajanefearn/financingapplication/Menu.java
@@ -1,41 +1,64 @@
 package emmajanefearn.financingapplication;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Menu extends ActionBarActivity {
-
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        addListenerOnButton();
-    }
+        //Declare Buttons
+        Button button = (Button) findViewById(R.id.button);
+        Button button6 = (Button) findViewById(R.id.button6);
 
-    public void addListenerOnButton() {
-
-        final Context context = this;
-
-        button = (Button) findViewById(R.id.button);
-
-        button.setOnClickListener(new OnClickListener() {
-
+        //On click of a buttons listener
+        View.OnClickListener listener = new View.OnClickListener() {
             @Override
-            public void onClick(View arg0) {
-
-                Intent intent = new Intent(context, Overview.class);
-                startActivity(intent);
-
+            public void onClick(View v) {
+                Log.d("TAG", String.valueOf(v.getId()));
+                Log.d("TAG", String.valueOf(R.id.button));
+                switch (v.getId()) {
+                    case R.id.button:
+                        Log.d("TAG", "Button 1");
+                        Intent intent = new Intent(Menu.this, Overview.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.button6:
+                        Log.d("TAG", "Button 2");
+                        Intent intent2 = new Intent(Menu.this, LogExpenditure.class);
+                        startActivity(intent2);
+                        break;
+                }
             }
+        };
 
-        });
+        //Set their listener for some reason idk
+        button.setOnClickListener(listener);
+        button6.setOnClickListener(listener);
     }
+       /* public void onClick(View v){
+            Log.d("TAG", String.valueOf(v.getId()));
+            Log.d("TAG", String.valueOf(R.id.button));
+            switch (v.getId()) {
+                case R.id.button:
+                    Log.d("TAG", "test");
+                    Intent intent = new Intent(Menu.this, Overview.class);
+                    Log.d("TAG", "hello");
+                    startActivity(intent);
+                    Log.d("TAG", "world");
+                    break;
+                case R.id.button6:
+                    Intent intent2 = new Intent(Menu.this, LogExpenditure.class);
+                    startActivity(intent2);
+                    break;
+                }
+    }*/
 }
+
